9f77758ed5b9823596fe3bd6e51382d894d2b90b
Aidana <zholdassova.aidana@mail.ru>
2021-11-19 22:57:25 +0600

    test by task3

---
+++ src/test/java/lesson3/WikiTest.java
@@ -0,0 +1,30 @@
+package lesson3;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WikiTest {
+
+    @Test
+    public void findCodeForJUnit5() {
+        // Открыть страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+
+        // Перейти в раздел Wiki проекта
+        $("#wiki-tab").click();
+
+        // Убедиться, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").click();
+        $("#wiki-pages-filter").setValue("so");
+
+        // Открыть страницу SoftAssertions
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+
+        // Проверить, что внутри есть пример кода для JUnit5
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
